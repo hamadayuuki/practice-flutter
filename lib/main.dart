10110b0d5121f9445b0d2ae708cc61a7eda4da8c
H-Yuuki <yuukirinnma@gmail.com>
2022-06-27 10:13:41 +0900

    add: TODOアプリの穴埋め用プログラム

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';   // Widget を使うためのライブラリ
 
+// アプリ起動時に呼び出される
 void main() {
   runApp(const MyTodoApp());
 }
 
 
+// 初めに表示する画面を指定する
 class MyTodoApp extends StatelessWidget {
   const MyTodoApp({Key? key}) : super(key: key);
 
@@ -13,9 +15,8 @@ class MyTodoApp extends StatelessWidget {
     return MaterialApp(
       title: 'My Todo App',   // アプリ名
       theme: ThemeData(
-        primarySwatch: Colors.green,   // テーマカラー
+        primarySwatch: Colors.blue,   // テーマカラー
       ),
-      
       // リスト一覧画面を表示
       home: const TodoListPage(),   // 起動時に表示する画面
     );
@@ -23,6 +24,7 @@ class MyTodoApp extends StatelessWidget {
 }
 
 
+// UIでの入力内容を変数へ同期させるために必要, 魔法の言葉
 class TodoListPage extends StatefulWidget {
   const TodoListPage({Key? key}) : super(key: key);
 
@@ -31,34 +33,74 @@ class TodoListPage extends StatefulWidget {
 }
 
 
+// リスト一覧画面
 class _TodoListPageState extends State<TodoListPage> {
 
   // TODOリストのデータ
-  List<String> todoList = ["あいうえお", "かきくけこ", "さしすせそ", "たちつてと"];
+  List<String> todoList = [];
+  String _text = "";   // 入力した文字が入る
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      // アプリバー(画面上)
       appBar: AppBar(
-        title: Text("TODOアプリ"),
+        title: Text("リスト一覧"),
       ),
 
+      // 画面
       body: Center(
+        
+        // TODOリスト表 + 文字入力 の縦並び
+        child: Column(
+          children: [
+            
+            // TODOリスト表
+            Expanded(
+              flex: 1,
+              
+              // 【TODOリストを表示】
+              child: 
+              
+              // ===========
+              
+            ),// Expanded
+            
+            // 文字入力画面・リスト追加ボタン
+            Expanded(
+              flex: 1,
+              child: Column(
+                
+                children: [
 
-        child: ListView.builder(
-          itemCount: todoList.length,   // リスト数 (=配列の要素数)
-          itemBuilder: (context, index) {
-            return Card(
-              child: ListTile(
-                title: Text(todoList[index]),
+                  // 文字入力画面
+                  TextField(
+                    onChanged: (String inputString) {
+                      setState( (){
+                        // 【入力された文字を _text へ代入】
+                        
+                        // ==========
+                      });
+                    }
+                  ),
+
+                  // 余白
+                  const SizedBox(height: 8),
+
+                  // 【リスト追加ボタン(+ todoListへの要素追加)】
+                  
+                  // ==========
+
+                  // 余白
+                  ,const SizedBox(height: 8),
+
+                ]
               )
-            );// Card
-          }
-        )// ListView.builder
+            ),// Expanded
+          ]
+        )
+      ),// Center
 
-      ),
-
-    );// Scaffold
+    );
   }
 }
